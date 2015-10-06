af50fb303583d48f15a0be4a542df3d499ee9d4e
kozakdl <dan.kozak@rainhail.com>
2015-10-06 15:16:31 -0500

    Branch change 2

--- src/com/kozakdl/sandbox/CreatedOnBranch.java
+++ src/com/kozakdl/sandbox/CreatedOnBranch.java
@@ -3,6 +3,6 @@ package com.kozakdl.sandbox;
 public class CreatedOnBranch {
     public static void main(String[] args) {
         System.out.println("This class created on the branch");
-        System.out.println("Branch change 1");
+        System.out.println("Branch change 2");
     }
 }
