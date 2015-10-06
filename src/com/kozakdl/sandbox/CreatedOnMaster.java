f571c8088ba9adee3e395ebf1c93104255023fa4
kozakdl <dan.kozak@rainhail.com>
2015-10-06 15:24:09 -0500

    Changes on master 1

--- src/com/kozakdl/sandbox/CreatedOnMaster.java
+++ src/com/kozakdl/sandbox/CreatedOnMaster.java
@@ -4,6 +4,7 @@ public class CreatedOnMaster {
 
 	public static void main(String[] args) {
 		System.out.println("This class created on the master.");
+		System.out.println("Master change 1");
 	}
 
 }
---
+++ src/com/kozakdl/sandbox/CreatedOnMaster2.java
@@ -0,0 +1,7 @@
+package com.kozakdl.sandbox;
+
+public class CreatedOnMaster2 {
+	public static void main(String[] args) {
+		System.out.println("created on master 2");
+	}
+}
