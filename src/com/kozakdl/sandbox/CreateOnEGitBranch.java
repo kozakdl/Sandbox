b4f214bdfee5850dc097540c033af9a323b43977
kozakdl <dan.kozak@rainhail.com>
2015-10-07 13:39:56 -0500

    Made changes on egit branch

---
+++ src/com/kozakdl/sandbox/CreateOnEGitBranch.java
@@ -0,0 +1,9 @@
+package com.kozakdl.sandbox;
+
+public class CreateOnEGitBranch {
+
+    public static void main(String[] args) {
+        System.out.println("Created on example-branch-egit");
+    }
+
+}
