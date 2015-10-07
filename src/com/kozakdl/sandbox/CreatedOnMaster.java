5df96930776a52b089d8c168e8125fec502049c5
kozakdl <dan.kozak@rainhail.com>
2015-10-07 08:47:16 -0500

    Make some changes on the master that will be merged into other branches

--- src/com/kozakdl/sandbox/CreatedOnMaster.java
+++ src/com/kozakdl/sandbox/CreatedOnMaster.java
@@ -5,6 +5,7 @@ public class CreatedOnMaster {
 	public static void main(String[] args) {
 		System.out.println("This class created on the master.");
 		System.out.println("Master change 3");
+        System.out.println("Change on master with conflict on example-branch-1");
 	}
 
 }
