796e5a92ec8329a03a1cf55cbe6c75faae618252
kozakdl <dan.kozak@rainhail.com>
2015-10-07 08:43:51 -0500

    Make first changes on example-branch-1

---
+++ src/com/kozakdl/sandbox/CreatedOnExampleBranch1.java
@@ -0,0 +1,9 @@
+package com.kozakdl.sandbox;
+
+public class CreatedOnExampleBranch1 {
+
+    public static void main(String[] args) {
+        System.out.println("Created on Example Branch 1!");
+    }
+
+}
