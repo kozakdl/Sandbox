ee8b46101315d3eb5da22f16a95b7b5304113ec3
kozakdl <dan.kozak@rainhail.com>
2015-10-07 10:20:01 -0500

    Make a change on example-branch-1

--- src/com/kozakdl/sandbox/CreatedOnExampleBranch1.java
+++ src/com/kozakdl/sandbox/CreatedOnExampleBranch1.java
@@ -4,6 +4,7 @@ public class CreatedOnExampleBranch1 {
 
     public static void main(String[] args) {
         System.out.println("Created on Example Branch 1!");
+        System.out.println("Updated on example-brance-1");
     }
 
 }
