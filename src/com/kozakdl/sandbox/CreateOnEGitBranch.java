922290ffa0f4d2cc7f19a4227049b5fc9878523f
kozakdl <dan.kozak@rainhail.com>
2015-10-08 09:03:42 -0500

    Merge example-branch-egit into master 201510080903

--- src/com/kozakdl/sandbox/CreateOnEGitBranch.java
+++ src/com/kozakdl/sandbox/CreateOnEGitBranch.java
@@ -4,6 +4,7 @@ public class CreateOnEGitBranch {
 
     public static void main(String[] args) {
         System.out.println("Created on example-branch-egit");
+        System.out.println("Modify on example-branch-egit");
     }
 
 }
